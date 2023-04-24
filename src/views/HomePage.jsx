55460dd60eb94fb12bde8d5bc5599e655e4bbb4c
Ali G.Jahed <aligjahed@gmail.com>
2023-04-24 18:58:29 +0330

    Initial commit

---
+++ src/views/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="h-screen bg-dark-dark_blue">
+      <p>This is a test</p>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    colors: {
+      dark: {
+        dark_blue: "#0F0E15",
+        light_blue: "#7873D1",
+        pale_blue: "#7873D1",
+        white: "#E0E3ED",
+      },
+    },
+  },
+};
